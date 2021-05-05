fb039dd92d55bc3ba6290eb8bc061371474c0623
chirag dongre <chiragdongre@gmail.com>
2021-05-05 11:28:52 +0530

    added tracker

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { DatePipe } from '@angular/common'
 
 
 @NgModule({
@@ -12,8 +14,11 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [DatePipe]
+
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DatePipe } from '@angular/common'
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,58 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  post :any;
+  date: any;
+  pincode:any;
+  dayslist:any;
+  posta:any;
+  vaccine:any;
+  dayList: any[] = [];
+  totalmap = new Map(); 
+  constructor(private http:HttpClient,public datepipe: DatePipe) {}
+  getData(code,noDays) {
+    if(code.length > 0 && noDays != null ){
+    this.pincode = code;
+    // this.date="04-05-2021";
+    // this.dayslist=[1,2]
+    let today = new Date()
+    const tomorrow = new Date(today)
+    for(let u = 1; u <= noDays; u++){
+    const tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate() + u)
+    let latest_date =this.datepipe.transform(tomorrow, 'dd-MM-yyyy');
+    this.dayList.push(latest_date);
 
-  constructor() {}
+  }
+    for (let i = 0; i < this.dayList.length; i++) {
+    // if(i ==1){
+    //   this.pincode = "421306";
+    // }else{
+    //   this.pincode = code;
+
+    // }
+    this.http.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=" + this.pincode + "&date="+this.dayList[i])
+    .subscribe((data) => {
+      if(data["centers"].length > 0){
+      this.post = JSON.stringify(data["centers"][0]["sessions"][0]["slots"]);
+      this.posta = JSON.stringify(data["centers"][0]["name"]);
+      this.vaccine = JSON.stringify(data["centers"][0]["sessions"][0]["vaccine"]);
+
+      this.totalmap.set(this.dayList[i], [this.posta,this.post,this.vaccine]);    
+      }else{
+
+        this.totalmap.set(this.dayList[i], ["Not Available","Not Available","Not Available"]);
+
+      } 
+
+      console.log(this.totalmap);
+    });
+  }
+  }else{
+
+alert("Please enter the Pin code and Number of days");
+
+  }
+  }
 
 }
